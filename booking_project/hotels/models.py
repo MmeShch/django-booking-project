c151d2381a535b3595735b5a36b212776e219370
natallia <3112shch@gmail.com>
2026-01-06 23:43:01 +0300

    add new tables for models

--- booking_project/hotels/models.py
+++ booking_project/hotels/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.text import slugify
 
 class Hotel(models.Model):
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True, db_index=True)
     location = models.CharField(max_length=200)
-    description = models.TextField()
-    photos = models.ImageField(upload_to='hotels/', blank=True)
+    description = models.TextField(blank=True)
+    main_photo = models.ImageField(upload_to='hotels/', blank=True, null=True)
     rating = models.DecimalField(
         max_digits=3,
         decimal_places=2,
@@ -20,11 +22,25 @@ class Hotel(models.Model):
 
     class Meta:
         db_table = 'hotels'
-        
+        ordering = ['name']
 
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+
+class HotelPhoto(models.Model):
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE, related_name='photos')
+    image = models.ImageField(upload_to='hotels/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Photo for {self.hotel.name}'
 
 
 
@@ -44,13 +60,13 @@ class Room(models.Model):
         db_column='hotel_id'
     )
     room_type = models.CharField(max_length=20, choices=ROOM_TYPES)
-    photos = models.ImageField(upload_to='rooms/', blank=True)
+    main_photo = models.ImageField(upload_to='rooms/', blank=True, null=True)
     price_per_night = models.DecimalField(
         max_digits=10,
         decimal_places=2,
         validators=[MinValueValidator(0)]
     )
-    available = models.BooleanField(default=True)
+    is_available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(null=True, blank=True)
@@ -59,8 +75,17 @@ class Room(models.Model):
 
     class Meta:
         db_table = 'rooms'
+        ordering = ['hotel', 'room_type']
 
 
     def __str__(self):
         return f"{self.hotel.name} - {self.get_room_type_display()} #{self.id}"
 
+
+class RoomPhoto(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='photos')
+    image = models.ImageField(upload_to='rooms/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Photo for room {self.id} in {self.room.hotel.name}'
