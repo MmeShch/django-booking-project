427c0fcfb1da50c3a7579647ce3a711f69f79877
natallia <3112shch@gmail.com>
2026-01-07 16:35:36 +0300

    add table Review to models + slug + register new tables in admin

--- booking_project/hotels/admin.py
+++ booking_project/hotels/admin.py
@@ -1,21 +1,46 @@
 from django.contrib import admin
-from hotels.models import Hotel, Room
+from hotels.models import Hotel, Room, Review, HotelPhoto, RoomPhoto
+
+
+class HotelPhotoInline(admin.TabularInline):
+    model = HotelPhoto
+    extra = 1
+
+
+class RoomPhotoInline(admin.TabularInline):
+    model = RoomPhoto
+    extra = 1
+
 
 @admin.register(Hotel)
 class HotelAdmin(admin.ModelAdmin):
     list_display = ['name', 'location', 'rating', 'created_at']
-    list_display_links = ['name', 'location']
-    list_filter = ['rating', 'deleted']
+    list_filter = ['rating', 'created_at']
     search_fields = ['name', 'location']
     readonly_fields = ['created_at', 'updated_at']
+    inlines = [HotelPhotoInline]
+
+    def is_active(self, obj):
+        return not obj.is_deleted
+    is_active.boolean = True
 
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
-    list_display = ['hotel', 'get_room_type_display', 'price_per_night', 'available']
-    list_display_links = ['hotel', 'get_room_type_display']
-    list_filter = ['room_type', 'available', 'hotel__location', 'deleted']
-    search_fields = ['hotel__name']
+    list_display = ['__str__', 'price_per_night', 'is_available']
+    list_filter = ['room_type', 'is_available', 'hotel']
+    list_editable = ['is_available']
+    inlines = [RoomPhotoInline]
+    list_select_related = ['hotel']
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('hotel', 'user', 'rating', 'created_at')
+    list_filter = ('rating', 'hotel')
+    search_fields = ('hotel__name', 'user__username')
+
+
 
 
 
--- booking_project/hotels/models.py
+++ booking_project/hotels/models.py
@@ -1,3 +1,4 @@
+from django.conf.global_settings import AUTH_USER_MODEL
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils.text import slugify
@@ -89,3 +90,24 @@ class RoomPhoto(models.Model):
 
     def __str__(self):
         return f'Photo for room {self.id} in {self.room.hotel.name}'
+
+
+class Review(models.Model):
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE,
+                             related_name='reviews')
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE,
+                              related_name='reviews')
+    comment = models.TextField(blank=True)
+    rating = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    is_deleted = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('user', 'hotel')
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'Review {self.rating} by {self.user} for {self.hotel}'
