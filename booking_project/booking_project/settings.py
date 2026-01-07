8dfc92d011dbd8267cad6937373cbd0a1f285c02
natallia <3112shch@gmail.com>
2026-01-07 19:40:33 +0300

    add base templates + urls core

--- booking_project/booking_project/settings.py
+++ booking_project/booking_project/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 from email.policy import default
-
+import os
 from decouple import config
 from pathlib import Path
 
@@ -126,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
